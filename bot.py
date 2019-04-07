3ee5e1753e46aa738e842a5c7f4d8450063f7325
Kirk Gasper <wigasper@gmail.com>
2019-04-07 13:23:45 -0500

    moved log parse to function

--- bot.py
+++ bot.py
@@ -47,51 +47,56 @@ async def on_message(message):
         msg = "GPU memory util.: {}".format(re.search("\d*MiB\s/\s*\d*MiB", nvidia_stat).group())
         await client.send_message(message.channel, msg)
         
+    if message.content.startswith('!checklog'):
+        await parse_ssh_log()
+        
     if message.content.startswith('!byerobot'):
         msg = 'Bite my shiny metal ass!'
         await client.send_message(message.channel, msg)
         await client.logout()
-        
+
+async def parse_ssh_log():
+    today = datetime.now()
+    yesterday = datetime.now() - timedelta(1)
+
+    sshd_failed = []
+    #sshd_did = []
+    with open("/var/log/auth.log", "r") as log:
+    #with open("/Users/wigasper/Documents/bender/auth.log", "r") as log:
+        for line in log:
+            match_today = re.search("^{}  {}".format(today.strftime("%b"), 
+                                today.day), line)
+            match_yesterday = re.search("^{}  {}".format(yesterday.strftime("%b"), 
+                                yesterday.day), line)
+            if match_today or match_yesterday:
+                if re.search("sshd.*Failed", line):
+                    sshd_failed.append(line)
+                #if re.search("sshd.*Did", line):
+                #    sshd_failed.append(line)
+
+    failed_ips = []
+    for fail in sshd_failed:
+        match = re.search("\d*\.\d*\.\d*\.\d*", fail)
+        if match:
+            failed_ips.append(match.group())
+    failed_ips = list(dict.fromkeys(failed_ips))
+    
+    msg = "{} failed SSH login attempt(s) occurred in the past " \
+          "two days.".format(str(len(sshd_failed)))
+          
+    server = client.get_server(SERVER)
+    await client.send_message(server.get_channel(CHANNEL), msg)
+    if len(sshd_failed) > 0:
+        msg = "Failures originated from IP address(es):"
+        await client.send_message(server.get_channel(CHANNEL), msg)
+        for ip in failed_ips:
+            await client.send_message(server.get_channel(CHANNEL), ip)
+
 async def send_interval_message():
     await client.wait_until_ready()
     interval = 86400
     while not client.is_closed:
-        today = datetime.now()
-        yesterday = datetime.now() - timedelta(1)
-    
-        sshd_failed = []
-        #sshd_did = []
-        with open("/var/log/auth.log", "r") as log:
-        #with open("/Users/wigasper/Documents/bender/auth.log", "r") as log:
-            for line in log:
-                match_today = re.search("^{}  {}".format(today.strftime("%b"), 
-                                    today.day), line)
-                match_yesterday = re.search("^{}  {}".format(yesterday.strftime("%b"), 
-                                    yesterday.day), line)
-                if match_today or match_yesterday:
-                    if re.search("sshd.*Failed", line):
-                        sshd_failed.append(line)
-                    #if re.search("sshd.*Did", line):
-                    #    sshd_failed.append(line)
-
-        failed_ips = []
-        for fail in sshd_failed:
-            match = re.search("\d*\.\d*\.\d*\.\d*", fail)
-            if match:
-                failed_ips.append(match.group())
-        failed_ips = list(dict.fromkeys(failed_ips))
-        
-        msg = "{} failed SSH login attempt(s) occurred in the past " \
-              "two days.".format(str(len(sshd_failed)))
-              
-        server = client.get_server(SERVER)
-        await client.send_message(server.get_channel(CHANNEL), msg)
-        if len(sshd_failed) > 0:
-            msg = "Failures originated from IP address(es):"
-            await client.send_message(server.get_channel(CHANNEL), msg)
-            for ip in failed_ips:
-                await client.send_message(server.get_channel(CHANNEL), ip)
-            
+        await parse_ssh_log()            
         await asyncio.sleep(interval)
 
 @client.event
